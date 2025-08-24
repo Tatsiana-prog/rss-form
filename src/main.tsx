0f4b2ff712d6da507023d4a450faab519b14d64c
Tatsiana-prog <programvtv07@gmail.com>
2025-08-24 23:55:57 +0300

    feat:add img for pr

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { store } from '../src/store/store';
+import { createRoot } from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import { store } from '../src/store/store';
-
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
-    <StrictMode>
+    <React.StrictMode>
       <Provider store={store}>
+        <BrowserRouter basename="/rss-form">
         <App />
+        </BrowserRouter>
       </Provider>
-    </StrictMode>
+    </React.StrictMode>
   );
 } else {
   console.error('Root element not found');
